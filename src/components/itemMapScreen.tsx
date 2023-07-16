d295a169336b035a33ffebcf5a0b1a53c2da40df
Kledio Myrto <klediomyrto24@gmail.con>
2023-07-16 23:26:12 +0200

    additions

--- src/components/itemMapScreen.tsx
+++ src/components/itemMapScreen.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import { StyleSheet, View } from "react-native";
 import Mapbox from "@rnmapbox/maps";
 
-Mapbox.setAccessToken("sk.eyJ1Ijoia2xlZGlvbTMiLCJhIjoiY2xqazA3OHljMGlqbDNkbnVqM3NpdGRraSJ9.fwFZ4Q8nXZ8PWtu3zos7EA");
+Mapbox.setAccessToken("sk.eyJ1Ijoia2xlZGlvbTMiLCJhIjoiY2xrMzRocHY3MDlyMTNqdGFvOWY1cTcwbyJ9.yDJfCMaqjZdFsobXo-0xkg");
 
 const ItemMapScreen = () => {
   return (
     <View style={styles.page}>
       <View style={styles.container}>
         <Mapbox.MapView style={styles.map} scaleBarEnabled={false}>
-          <Mapbox.Camera zoomLevel={15} centerCoordinate={[19.818967, 41.3276779]} animationDuration={0}/>
+          <Mapbox.Camera zoomLevel={15} centerCoordinate={[19.818967, 41.3276779]} animationDuration={0} />
           {/* <Mapbox.PointAnnotation id="point" coordinate={[19.819025, 41.327953]} /> */}
         </Mapbox.MapView>
       </View>
